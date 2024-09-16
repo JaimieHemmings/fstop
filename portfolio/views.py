56c4e4c42f7fd55d5c74aabf64411fe8cd837b77
Jaimie <jaimie.j.hemmings@gmail.com>
2024-09-16 17:16:54 +0100

    add: portfolio page template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def portfolio(request):
+    """
+    A view to return the portfolio page
+    """
+    return render(request, 'portfolio.html')
