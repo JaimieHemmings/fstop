5ce55742240d45b41b5130fcd02b2317591b35f7
Jaimie <jaimie.j.hemmings@gmail.com>
2024-09-16 18:21:06 +0100

    add: article model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    body = models.TextField()
+    body_continued = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    thumb = models.ImageField(default='default.png', blank=True)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    cover_image = models.ImageField(default='default.png', blank=True)
+    body_image = models.ImageField(default='default.png', blank=True)
+    last_modified = models.DateTimeField(auto_now=True)
--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
+from blog.models import Article
 from .models import Message
 
 class MessageAdmin(admin.ModelAdmin):
     pass
 
+class ArticleAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Article, ArticleAdmin)
