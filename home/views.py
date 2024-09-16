97072f79029053dfbfa4adadfc299b811c928e82
Jaimie <jaimie.j.hemmings@gmail.com>
2024-09-16 14:59:22 +0100

    add: additional structure, templates and styling

--- home/views.py
+++ home/views.py
@@ -5,3 +5,15 @@ def index(request):
     A view that displays the index page
     """
     return render(request, 'home/index.html')
+
+def about(request):
+    """
+    A view that displays the about page
+    """
+    return render(request, 'home/about.html')
+
+def contact(request):
+    """
+    A view that displays the contact page
+    """
+    return render(request, 'home/contact.html')
