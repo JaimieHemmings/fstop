89818b8c5a63c7ec98b86dc8b3d548c46108b73a
Jaimie <jaimie.j.hemmings@gmail.com>
2024-09-23 11:32:27 +0100

    chore: payments model update

--- payments/models.py
+++ payments/models.py
@@ -11,6 +11,7 @@ class Payment(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
     paid = models.BooleanField(default=False)
+    paid_date = models.DateTimeField(null=True, blank=True)
     stripe_id = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField()
 
