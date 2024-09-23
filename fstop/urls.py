58f2e1740db63fb0640bda4363f584100f7d42f0
Jaimie <jaimie.j.hemmings@gmail.com>
2024-09-23 16:20:34 +0100

    chore: edited urls.py

--- fstop/urls.py
+++ fstop/urls.py
@@ -14,13 +14,4 @@ urlpatterns = [
     path('control-panel/', include('controlpanel.urls')),
     path('payments/', include('payments.urls')),
     path('profile/', include('profiles.urls')),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns += (
-        re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-        re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-        )
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
