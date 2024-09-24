ccf032d5ef644825a69dda2d187952fb96d1affe
Jaimie <jaimie.j.hemmings@gmail.com>
2024-09-24 16:00:33 +0100

    Update settings.py

--- fstop/settings.py
+++ fstop/settings.py
@@ -1,5 +1,6 @@
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
