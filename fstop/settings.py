0f51bfd67f505c2a73eed533ea1be2fe4bb1bfd7
Jaimie <jaimie.j.hemmings@gmail.com>
2024-09-25 10:16:44 +0100

    fix: typo

--- fstop/settings.py
+++ fstop/settings.py
@@ -173,7 +173,7 @@ if 'USE_AWS' in os.environ:
 
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_S3_REGION_NAME = 'eu-noth-1'
+    AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     # Static and media files
