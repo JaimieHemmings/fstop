3ae1221fcfe12fd5f51bd665f55495f80bb656d4
Jaimie <jaimie.j.hemmings@gmail.com>
2024-09-26 08:02:31 +0100

    add: stripe webhook

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from webhooks.py import webhook
 
 urlpatterns = [
     path("", views.profile, name="profile"),
     path("edit-profile/", views.edit_profile, name="edit_profile"),
     path("make-payment/<int:id>", views.make_payment, name="make_payment"),
     path("payment-success/<int:id>", views.payment_success, name="payment_success"),
+    path("wh/", webhook, name="webhook"),
 ]
