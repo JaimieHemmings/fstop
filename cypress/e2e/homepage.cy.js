6b9e89ef384012dd4ddbfc10440c23e5ce018a73
Jaimie <jaimie.j.hemmings@gmail.com>
2024-10-06 12:35:29 +0100

    add: additional testing functionality

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,5 +1,25 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('test homepage functionality', () => {
+  beforeEach(() => {
     cy.visit('/')
+  })
+  it('Has H1 with valid length and is visible', () => {
+    // Test H1 exists with length greater than 0
+    cy.get('h1')
+      .should('exist')
+      .and('be.visible')
+      .should('have.length', 1)
+      .invoke('text').should('have.length.gt', 0)
+  })
+
+  it('Has an accordian and functions correctly', () => {
+    // Test accordion functionality
+    cy.getDataCy('accordion').should('exist')
+    cy.contains('Photography is a totally bespoke service').should('be.visible')
+    cy.getDataCy('faq-item-3-heading').click()
+    cy.contains('Photography is a totally bespoke service').should('not.be.visible')
+    cy.contains('Yes, all of our photos are edited to ensure that they are of the highest quality').should('not.be.visible')
+    cy.getDataCy('faq-item-4-heading').click()
+    cy.contains('Yes, all of our photos are edited to ensure that they are of the highest quality').should('be.visible')
+    cy.getDataCy('faq-item-6-heading').click()
   })
 })
--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -12,6 +12,27 @@ describe('Test Navigation', () => {
     cy.visit('/contact')
     cy.url().should('include', '/contact')
 
+    // Test Dropdown nav item works
+    cy.contains('Login').should('not.be.visible')
+    cy.getDataCy('top-nav').within(() => {
+      cy.getDataCy('drop-down-nav').click()
+    })
+    cy.contains('Login').should('be.visible')
+    cy.getDataCy('top-nav').within(() => {
+      cy.getDataCy('drop-down-nav').click()
+    })
+    cy.contains('Login').should('not.be.visible')
+
+    // Test Login and Register Links
+    cy.getDataCy('top-nav').within(() => {
+      cy.getDataCy('drop-down-nav').click()
+      cy.getDataCy('login-button').click()
+      cy.contains('Login').should('not.be.visible')
+      cy.getDataCy('drop-down-nav').click()
+      cy.getDataCy('register-button').click()
+    })
+
+
     // Test 404 works
     cy.request({url: '/404', failOnStatusCode: false}).its('status').should('eq', 404)
   })
