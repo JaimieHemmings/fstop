b296edf7ff536d6466a6fd84c21ec60021736a5c
Jaimie <jaimie.j.hemmings@gmail.com>
2024-10-08 15:48:45 +0100

    add: cms consistency message display

--- controlpanel/views/cms_portfolio_views.py
+++ controlpanel/views/cms_portfolio_views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect, get_object_or_404, reverse
 
+from home.models import Message
 from portfolio.models import PortfolioImages
 
 from controlpanel.forms import AddPortfolioImage
@@ -12,11 +13,15 @@ def cp_portfolio(request):
     """
     A view to return the portfolio page
     """
-    context = {}
+    
+    total_unread_messages = Message.objects.filter(read=False).count()
+    unread_messages = Message.objects.filter(read=False)[:5]
     portfolio_images = PortfolioImages.objects.all()
     
     context = {
         "portfolio_images": portfolio_images,
+        "total_unread_messages": total_unread_messages,
+        "unread_messages": unread_messages
     }
     return render(request, "portfolio/portfolio-management.html", context)
 
@@ -26,6 +31,9 @@ def add_portfolio_image(request):
     """
     A view to add a slider image
     """
+    total_unread_messages = Message.objects.filter(read=False).count()
+    unread_messages = Message.objects.filter(read=False)[:5]
+
     form = AddPortfolioImage()
     if request.method == "POST":
         form = AddPortfolioImage(request.POST, request.FILES)
@@ -38,6 +46,8 @@ def add_portfolio_image(request):
         "form": form,
         "page_title": "Add Portfolio Image",
         "end_point": "add_portfolio_image",
+        "total_unread_messages": total_unread_messages,
+        "unread_messages": unread_messages
     }
     return render(request, "generic/add-item.html", context)
 
@@ -47,9 +57,15 @@ def delete_portfolio_image_confirm(request, image_id):
     """
     A view to confirm the deletion of a portfolio image
     """
-    context = {}
     image = get_object_or_404(PortfolioImages, id=image_id)
-    context["image"] = image
+    total_unread_messages = Message.objects.filter(read=False).count()
+    unread_messages = Message.objects.filter(read=False)[:5]
+
+    context = {
+        "image": image,
+        "total_unread_messages": total_unread_messages,
+        "unread_messages": unread_messages
+    }    
 
     return render(
         request, "portfolio/delete-portfolio-image-confirm.html", context)
@@ -61,6 +77,7 @@ def delete_portfolio_image(request, image_id):
     A view to delete a portfolio image
     """
     image = get_object_or_404(PortfolioImages, id=image_id)
+
     image.delete()
     messages.success(request, "Portfolio image deleted successfully")
     return redirect(reverse("cp_portfolio"))
--- controlpanel/views/cms_reviews_views.py
+++ controlpanel/views/cms_reviews_views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect, get_object_or_404, reverse
 
 from reviews.models import Review
+from home.models import Message
 
 from controlpanel.forms import AddReviewForm
 
@@ -13,9 +14,14 @@ def cms_manage_reviews(request):
     """
     A view to return the reviews page
     """
+    total_unread_messages = Message.objects.filter(read=False).count()
+    unread_messages = Message.objects.filter(read=False)[:5]
     reviews = Review.objects.all()
+
     context = {
         "reviews": reviews,
+        "total_unread_messages": total_unread_messages,
+        "unread_messages": unread_messages,
     }
     return render(request, "cms/reviews/cms-reviews.html", context)
 
@@ -25,15 +31,23 @@ def cms_add_review(request):
     """
     A view to add a review
     """
+    total_unread_messages = Message.objects.filter(read=False).count()
+    unread_messages = Message.objects.filter(read=False)[:5]
     form = AddReviewForm()
-    context = {}
+
     if request.method == "POST":
         form = AddReviewForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             messages.success(request, "Review added successfully")
             return redirect(cms_manage_reviews)
-    context["form"] = form
+        
+    context = {
+        "form": form,
+        "total_unread_messages": total_unread_messages,
+        "unread_messages": unread_messages
+    }
+
     return render(request, "cms/reviews/cms-add-review.html", context)
 
 
@@ -42,9 +56,16 @@ def cms_delete_review_confirm(request, review_id):
     """
     A view to confirm the deletion of a review
     """
-    context = {}
+    total_unread_messages = Message.objects.filter(read=False).count()
+    unread_messages = Message.objects.filter(read=False)[:5]
     review = get_object_or_404(Review, id=review_id)
-    context["review"] = review
+
+    context = {
+        "review": review,
+        "total_unread_messages": total_unread_messages,
+        "unread_messages": unread_messages
+    }
+
     return render(
         request, "cms/reviews/cms-delete-review-confirm.html", context)
 
@@ -65,15 +86,24 @@ def cms_edit_review(request, review_id):
     """
     A view to edit a review
     """
-    context = {}
+    total_unread_messages = Message.objects.filter(read=False).count()
+    unread_messages = Message.objects.filter(read=False)[:5]
     review = get_object_or_404(Review, id=review_id)
     form = AddReviewForm(instance=review)
-    context["review"] = review
+
+    
     if request.method == "POST":
         form = AddReviewForm(request.POST, request.FILES, instance=review)
         if form.is_valid():
             form.save()
             messages.success(request, "Review updated successfully")
             return redirect(reverse("cms_manage_reviews"))
-    context["form"] = form
+    
+    context = {
+        "form": form,
+        "review": review,
+        "total_unread_messages": total_unread_messages,
+        "unread_messages": unread_messages
+    }
+    
     return render(request, "cms/reviews/cms-edit-review.html", context)
