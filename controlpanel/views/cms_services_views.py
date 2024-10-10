426d1371eff9900bdd22bfa7320c7285fafc3d7e
Jaimie <jaimie.j.hemmings@gmail.com>
2024-10-10 12:10:32 +0100

    add: refactor services page

--- controlpanel/views/cms_services_views.py
+++ controlpanel/views/cms_services_views.py
@@ -3,19 +3,6 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect, get_object_or_404, reverse
 
 from home.models import Message
-from services.models import (
-    ServicesHero,
-    ServicesBanner,
-    ServicesCards,
-    ServicesContextBannerOne,
-    ServicesContextBannerTwo,
-)
-
-from services.forms import (
-  ServicesHeroForm,
-  ServicesBannerForm,
-  ServicesCardsForm,
-)
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -35,22 +22,7 @@ def cp_cms_edit_services_hero(request):
     unread_messages = Message.objects.filter(read=False)[:5]
     total_unread_messages = Message.objects.filter(read=False).count()
 
-    hero_image = ServicesHero.objects.get(id=1)
-    form = ServicesHeroForm(instance=hero_image)
-
-    if request.method == "POST":
-        form = ServicesHeroForm(
-            request.POST, request.FILES, instance=hero_image)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Hero image updated successfully")
-            return redirect(reverse("cp_cms_manage_services"))
-        else:
-            messages.error(
-                request, "There was an error updating the hero image")
-
     context = {
-        "form": form,
         "unread_messages": unread_messages,
         "total_unread_messages": total_unread_messages,
     }
@@ -62,20 +34,7 @@ def cp_cms_edit_services_banner(request):
     unread_messages = Message.objects.filter(read=False)[:5]
     total_unread_messages = Message.objects.filter(read=False).count()
 
-    banner = ServicesBanner.objects.get(id=1)
-    form = ServicesBannerForm(instance=banner)
-
-    if request.method == "POST":
-        form = ServicesBannerForm(request.POST, request.FILES, instance=banner)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Banner updated successfully")
-            return redirect(reverse("cp_cms_manage_services"))
-        else:
-            messages.error(request, "There was an error updating the banner")
-
     context = {
-        "form": form,
         "unread_messages": unread_messages,
         "total_unread_messages": total_unread_messages,
     }
@@ -87,12 +46,9 @@ def cp_cms_edit_services_cards(request):
     unread_messages = Message.objects.filter(read=False)[:5]
     total_unread_messages = Message.objects.filter(read=False).count()
 
-    cards = ServicesCards.objects.all()
-
     context = {
         "unread_messages": unread_messages,
         "total_unread_messages": total_unread_messages,
-        "cards": cards,
     }
     return render(request, "cms/services/service-cards.html", context)
 
@@ -102,23 +58,9 @@ def cp_cms_edit_services_card(request, card_id):
     unread_messages = Message.objects.filter(read=False)[:5]
     total_unread_messages = Message.objects.filter(read=False).count()
 
-    card = get_object_or_404(ServicesCards, id=card_id)
-    form = ServicesCardsForm(instance=card)
-
-    if request.method == "POST":
-        form = ServicesCardsForm(request.POST, request.FILES, instance=card)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Card updated successfully")
-            return redirect(reverse("cp_cms_edit_services_cards"))
-        else:
-            messages.error(request, "There was an error updating the card")
-
     context = {
-        "form": form,
         "unread_messages": unread_messages,
         "total_unread_messages": total_unread_messages,
-        "card": card,
     }
     return render(request, "cms/services/edit-service-card.html", context)
 
@@ -128,12 +70,9 @@ def cp_cms_delete_services_card_confirm(request, card_id):
     unread_messages = Message.objects.filter(read=False)[:5]
     total_unread_messages = Message.objects.filter(read=False).count()
 
-    card = get_object_or_404(ServicesCards, id=card_id)
-
     context = {
         "unread_messages": unread_messages,
         "total_unread_messages": total_unread_messages,
-        "card": card,
     }
     return render(request,
                   "cms/services/cms-delete-service-card-confirm.html",
@@ -142,8 +81,6 @@ def cp_cms_delete_services_card_confirm(request, card_id):
 
 @user_passes_test(lambda u: u.is_superuser)
 def cp_cms_delete_services_card(request, card_id):
-    card = get_object_or_404(ServicesCards, id=card_id)
-    card.delete()
     messages.success(request, "Card deleted successfully")
 
     return redirect(reverse("cp_cms_edit_services_cards"))
@@ -159,18 +96,6 @@ def cp_cms_add_services_card(request):
         "total_unread_messages": total_unread_messages,
     }
 
-    form = ServicesCardsForm()
-
-    if request.method == "POST":
-        form = ServicesCardsForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Card added successfully")
-            return redirect(reverse("cp_cms_edit_services_cards"))
-        else:
-            messages.error(request, "There was an error adding the card")
-
-    context["form"] = form
     return render(request,
                   "cms/services/cms-add-service-card.html",
                   context)
@@ -185,24 +110,12 @@ def cp_cms_edit_context_banner_one(request):
     unread_messages = Message.objects.filter(read=False)[:5]
     total_unread_messages = Message.objects.filter(read=False).count()
 
-    banner = get_object_or_404(ServicesContextBannerOne, id=1)
-    form = ServicesBannerForm(instance=banner)
-
-    if request.method == "POST":
-        form = ServicesBannerForm(request.POST, request.FILES, instance=banner)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Banner updated successfully")
-            return redirect(cp_cms_manage_services)
-        else:
-            messages.error(request, "There was an error updating the banner")
-
     context = {
-        "form": form,
         "unread_messages": unread_messages,
         "total_unread_messages": total_unread_messages,
     }
-    return render(request, "cms/services/cms-edit-context-banner-one.html",
+    return render(request,
+                  "cms/services/cms-edit-context-banner-one.html",
                   context)
 
 
@@ -211,22 +124,11 @@ def cp_cms_edit_context_banner_two(request):
     unread_messages = Message.objects.filter(read=False)[:5]
     total_unread_messages = Message.objects.filter(read=False).count()
 
-    banner = get_object_or_404(ServicesContextBannerTwo, id=1)
-    form = ServicesBannerForm(instance=banner)
-
-    if request.method == "POST":
-        form = ServicesBannerForm(request.POST, request.FILES, instance=banner)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Banner updated successfully")
-            return redirect(reverse("cp_cms_manage_services"))
-        else:
-            messages.error(request, "There was an error updating the banner")
-
     context = {
-        "form": form,
         "unread_messages": unread_messages,
         "total_unread_messages": total_unread_messages,
     }
-    return render(request, "cms/services/cms-edit-context-banner-two.html",
+
+    return render(request,
+                  "cms/services/cms-edit-context-banner-two.html",
                   context)
--- services/models.py
+++ services/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 import uuid
 
-class ServicesHero(models.Model):
+class ServicesPage(models.Model):
     """
-    A model to define the services hero image
+    A model to define the services page
     """
     def get_path(instance, filename):
         extension = filename.split(".")[-1]
@@ -11,73 +11,35 @@ class ServicesHero(models.Model):
         return f"services/hero/{filename}"
     
     title = models.CharField(max_length=100)
-    lead = models.CharField(max_length=500)
-    image = models.ImageField(upload_to=get_path)
-    alt_text = models.CharField(max_length=100)
+
+    hero_title = models.CharField(max_length=100)
+    hero_lead = models.CharField(max_length=500)
+    hero_image = models.ImageField(upload_to=get_path)
+    hero_alt_text = models.CharField(max_length=100)
+
+    banner_image = models.ImageField(upload_to=get_path)
+    banner_alt_text = models.CharField(max_length=100)
+
+    highlight_one_title = models.CharField(max_length=100)
+    highlight_one_lead = models.CharField(max_length=500)
+    highlight_one_image = models.ImageField(upload_to=get_path)
+    highlight_one_alt_text = models.CharField(max_length=100)
+
+    highlight_two_title = models.CharField(max_length=100)
+    highlight_two_lead = models.CharField(max_length=500)
+    highlight_two_image = models.ImageField(upload_to=get_path)
+    highlight_two_alt_text = models.CharField(max_length=100)
+
+    service_info_one_title = models.CharField(max_length=100)
+    service_info_one_lead = models.CharField(max_length=500)
+    service_info_one_image = models.ImageField(upload_to=get_path)
+    service_info_one_alt_text = models.CharField(max_length=100)
+
+    service_info_two_title = models.CharField(max_length=100)
+    service_info_two_lead = models.CharField(max_length=500)
+    service_info_two_image = models.ImageField(upload_to=get_path)
+    service_info_two_alt_text = models.CharField(max_length=100)
+    
 
     def __str__(self):
         return self.title
-    
-
-class ServicesBanner(models.Model):
-    """
-    A model to define the services banner image
-    """
-    def get_path(instance, filename):
-        extension = filename.split(".")[-1]
-        filename = f"{filename}-{uuid.uuid4()}.{extension}"
-        return f"services/banner/{filename}"
-    
-    image = models.ImageField(upload_to=get_path)
-    alt_text = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.title
-    
-
-class ServicesCards(models.Model):
-    """
-    A model to define the services cards
-    """    
-    title = models.CharField(max_length=100)
-    lead = models.CharField(max_length=500)
-    icon = models.CharField(max_length=2000)
-
-    def __str__(self):
-        return self.title
-    
-
-class ServicesContextBannerOne(models.Model):
-    """
-    A model to define the services context banner one
-    """
-    def get_path(instance, filename):
-        extension = filename.split(".")[-1]
-        filename = f"{filename}-{uuid.uuid4()}.{extension}"
-        return f"services/context/banner_one/{filename}"
-    
-    title = models.CharField(max_length=100)
-    lead = models.CharField(max_length=500)
-    image = models.ImageField(upload_to=get_path)
-    alt_text = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.title
-    
-
-class ServicesContextBannerTwo(models.Model):
-  """
-  A model to define the services context banner two
-  """
-  def get_path(instance, filename):
-      extension = filename.split(".")[-1]
-      filename = f"{filename}-{uuid.uuid4()}.{extension}"
-      return f"services/context/banner_two/{filename}"
-
-  title = models.CharField(max_length=100)
-  lead = models.CharField(max_length=500)
-  image = models.ImageField(upload_to=get_path)
-  alt_text = models.CharField(max_length=100)
-
-  def __str__(self):
-      return self.title
