65860093a9d6b4fc907138e0c80292cd41125845
Jaimie <jaimie.j.hemmings@gmail.com>
2024-10-11 14:26:49 +0100

    add: additional styling changes

--- home/tests.py
+++ home/tests.py
@@ -9,10 +9,28 @@ class UrlsTest(TestCase):
         path = reverse('home')
         print(f"Resolved Path: {path}")
 
+    def test_about_url(self):
+        path = reverse('about')
+        print(f"Resolved Path: {path}")
+
+    def test_contact_url(self):
+        path = reverse('contact')
+        print(f"Resolved Path: {path}")
+
 
 class HttpResponseTest(TestCase):
 
     def test_index_response(self):
         path = reverse('home')
+        response = requests.get(f"{BASE_URL}{path}")
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_response(self):
+        path = reverse('about')
+        response = requests.get(f"{BASE_URL}{path}")
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_response(self):
+        path = reverse('contact')
         response = requests.get(f"{BASE_URL}{path}")
         self.assertEqual(response.status_code, 200)
