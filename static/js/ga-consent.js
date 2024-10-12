de9c5f859b2cc3e6d21da843d1422ebb375f3419
Jaimie <jaimie.j.hemmings@gmail.com>
2024-10-12 16:21:44 +0100

    add: cookie consent functionality

---
+++ static/js/ga-consent.js
@@ -0,0 +1,26 @@
+function allConsentGranted() {
+  gtag('consent', 'update', {
+    'ad_user_data': 'granted',
+    'ad_personalization': 'granted',
+    'ad_storage': 'granted',
+    'analytics_storage': 'granted'
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  // Check if the user has already consented
+  if (document.cookie.indexOf("cookie_consent=accepted") > -1) {
+    allConsentGranted();
+    return;
+  } else {
+    document.getElementById("cookie-consent").classList.add("show");
+    // If the user clicks the consent button then remove the class show from the cookie consent div
+    document.getElementById("accept-cookies").addEventListener("click", function() {
+      document.getElementById("cookie-consent").classList.remove("show");
+      // then add a cookie to the user session to remember that the user has consented
+      document.cookie = "cookie_consent=accepted; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+      allConsentGranted();
+    });
+  }
+});
